ec1bdd8d4bcf989a599473b9132221fff1671860
pbmax91 <pbmax91@gmail.com>
2014-04-08 19:27:04 -0400

    lab10 initial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,17 +15,19 @@ def new
 end
 
 def create
-  @book = Book.new(book_params)
-  @book.save
-  redirect_to @book
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: "#{@book.title} was created!"
+    else
+      render :new
+    end
 end
-
-def edit
-end
-
 def update
-  @book.update(book_params)
-  redirect_to @book
+    if @book.update(book_params)
+      redirect_to @book, notice: "#{@book.title} was created!"
+    else
+      render :new
+    end
 end
 
 def destroy
