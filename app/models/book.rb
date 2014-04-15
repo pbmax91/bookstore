ea6c1a192bec427119b2259c159d08a4cda79e2d
pbmax91 <pbmax91@gmail.com>
2014-04-15 18:52:45 -0400

    class11lab1 initial push

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,5 @@ validates :pages,
 validates :price, 
     numericality: { greater_than_or_equal_to: 0 },
     if: "price.present?"
+has_many :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Bookstore::Application.routes.draw do
-  resources :books do
-    get 'page/:page', :action => :index, :on => :collection
+resources :books do
+  resources :reviews
+  get 'page/:page', :action => :index, :on => :collection
 end
-  root 'books#index'
+root 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
